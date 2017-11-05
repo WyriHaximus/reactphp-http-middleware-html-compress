ef428c1e3eec996640219ebdd13485b756548523
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-11-05 23:15:51 +0100

    HTML compress middleware for react/http

---
+++ src/HtmlCompressMiddleware.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\HttpBodyStream;
+use WyriHaximus\HtmlCompress\Factory;
+use WyriHaximus\HtmlCompress\Parser;
+use function React\Promise\resolve;
+use function RingCentral\Psr7\stream_for;
+
+final class HtmlCompressMiddleware
+{
+    const MIME_TYPES = [
+        'text/html',
+        'text/xhtml',
+    ];
+
+    /**
+     * @var Parser
+     */
+    private $compressor;
+
+    /**
+     * @param Parser $compressor
+     */
+    public function __construct(Parser $compressor = null)
+    {
+        if ($compressor === null) {
+            $compressor = Factory::constructFastest();
+        }
+
+        $this->compressor = $compressor;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        return resolve($next($request))->then(function (ResponseInterface $response) {
+            if ($response->getBody() instanceof HttpBodyStream) {
+                return $response;
+            }
+
+            if (!$response->hasHeader('content-type')) {
+                return $response;
+            }
+
+            list($contentType) = explode(';', $response->getHeaderLine('content-type'));
+            if (!in_array($contentType, self::MIME_TYPES, true)) {
+                return $response;
+            }
+
+            $body = (string)$response->getBody();
+            $compressedBody = $this->compressor->compress($body);
+
+            return $response->withBody(stream_for($compressedBody))->withHeader('Content-Length', strlen($compressedBody));
+        });
+    }
+}
---
+++ tests/HtmlCompressMiddlewareTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Tests\Http\Middleware;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Factory;
+use React\Http\Response;
+use React\Http\ServerRequest;
+use function Clue\React\Block\await;
+use function RingCentral\Psr7\stream_for;
+use WyriHaximus\React\Http\Middleware\HtmlCompressMiddleware;
+
+final class HtmlCompressMiddlewareTest extends TestCase
+{
+    public function provideContentTypes()
+    {
+        foreach (HtmlCompressMiddleware::MIME_TYPES as $contentType) {
+            yield [$contentType];
+        }
+    }
+
+    /**
+     * @dataProvider provideContentTypes
+     */
+    public function testCompressedResponse(string $contentType)
+    {
+        $request = (new ServerRequest('GET', 'https://example.com/'))->withBody(stream_for('foo.bar'));
+        $middleware = new HtmlCompressMiddleware();
+        /** @var ServerRequestInterface $compressedResponse */
+        $compressedResponse = await($middleware($request, function (ServerRequestInterface $request) use ($contentType) {
+            return new Response(
+                200,
+                [
+                    'Content-Type' => $contentType,
+                ],
+                '<head>
+                    <title>woei</title>
+                </head>'
+            );
+        }), Factory::create());
+        self::assertSame(32, (int)$compressedResponse->getHeaderLine('Content-Length'));
+        self::assertSame('<head><title>woei</title></head>', (string)$compressedResponse->getBody());
+    }
+
+    public function testIgnoreNotSupportedAndMissingContentTypes()
+    {
+        $body = '<head>
+                    <title>woei</title>
+                </head>';
+        $request = (new ServerRequest('GET', 'https://example.com/'))->withBody(stream_for('foo.bar'));
+        $middleware = new HtmlCompressMiddleware();
+        /** @var ServerRequestInterface $compressedResponse */
+        $compressedResponse = await($middleware($request, function (ServerRequestInterface $request) use ($body) {
+            return new Response(
+                200,
+                [],
+                $body
+            );
+        }), Factory::create());
+        self::assertSame($body, (string)$compressedResponse->getBody());
+    }
+}
